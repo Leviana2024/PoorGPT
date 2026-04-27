c0f26833618615dfdcbb68ae2bf295e3901e0296
LEVIANA <princejoaquineleonardo@gmail.com>
2026-04-28 00:48:31 +0800

    Refactor mint function and add baseURI support

--- PoorGPT.sol
+++ PoorGPT.sol
@@ -1,9 +1,9 @@
-import "@openzeppelin/contracts/utils/Strings.sol";
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PoorGPT is ERC721URIStorage, Ownable {
 
@@ -11,26 +11,30 @@ contract PoorGPT is ERC721URIStorage, Ownable {
     uint256 public maxSupply = 1000;
     uint256 public mintPrice = 0.01 ether;
 
-    constructor() ERC721("PoorGPT", "PGPT") {}
+    string public baseURI;
+
+    constructor() ERC721("PoorGPT", "PGPT") {
+        baseURI = "https://yourdomain.com/metadata/"; // 🔥 CHANGE THIS
+    }
 
     function mint() public payable {
         require(totalSupply < maxSupply, "Sold out");
         require(msg.value >= mintPrice, "Not enough ETH");
 
         totalSupply++;
+        uint256 tokenId = totalSupply;
 
-        _safeMint(msg.sender, totalSupply);
+        _safeMint(msg.sender, tokenId);
 
-        // metadata link (CHANGE THIS)
-        string memory uri = string(
-            abi.encodePacked(
-                "https://yourdomain.com/metadata/",
-                Strings.toString(totalSupply),
-                ".json"
-            )
+        string memory finalURI = string(
+            abi.encodePacked(baseURI, Strings.toString(tokenId), ".json")
         );
 
-        _setTokenURI(totalSupply, uri);
+        _setTokenURI(tokenId, finalURI);
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
     }
 
     function withdraw() public onlyOwner {
